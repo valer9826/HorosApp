621e703447f112dafcca2913b5c192bc2666dd19
Renato Valer <valer9826@gmail.com>
2023-10-08 22:44:04 -0500

    - add BottomNavView - add navpgrah to bottomNavView

--- app/src/main/java/com/example/horosapp/features/home/MainActivity.kt
+++ app/src/main/java/com/example/horosapp/features/home/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.horosapp.features.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horosapp.R
 import com.example.horosapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomMenu.setupWithNavController(navController)
+
     }
 }
