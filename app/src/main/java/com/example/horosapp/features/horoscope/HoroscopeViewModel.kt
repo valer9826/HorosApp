191f6f8e9c0941d15402865b91b00326cb831748
Renato Valer <valer9826@gmail.com>
2023-10-10 22:30:36 -0500

    update ui HoroscopeFragment

--- app/src/main/java/com/example/horosapp/features/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/example/horosapp/features/horoscope/HoroscopeViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.horosapp.features.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.horosapp.domain.horoscope.model.HoroscopeInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HoroscopeViewModel @Inject constructor(): ViewModel() {
 
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> get() = _horoscope
+
+    init {
+        _horoscope.value = listOf(
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini
+        )
+    }
 
 }
