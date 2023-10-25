ac273b399148346dc2513ba2e0db2e48fc18c9af
Renato Valer <valer9826@gmail.com>
2023-10-24 22:14:43 -0500

    Add DataSourceProvider

--- app/src/main/java/com/example/horosapp/data/di/DataSourceProvider.kt
+++ app/src/main/java/com/example/horosapp/data/di/DataSourceProvider.kt
@@ -1,17 +1,19 @@
 package com.example.horosapp.data.di
 
-import com.example.horosapp.data.remote.horoscopemodule.HoroscopeApi
 import com.example.horosapp.data.remote.horoscopemodule.HoroscopeDS
+import com.example.horosapp.data.remote.horoscopemodule.HoroscopeDSImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceProvider {
 
     @Binds
-    abstract fun bindHoroscopeServiceDS(horoscopeApi: HoroscopeApi): HoroscopeDS
+    @Singleton
+    abstract fun bindHoroscopeServiceDS(horoscopeDS: HoroscopeDSImpl): HoroscopeDS
 
 }
