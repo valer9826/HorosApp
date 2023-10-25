de38c304f7fda87b39219210eeed83d8d7ee965d
Renato Valer <valer9826@gmail.com>
2023-10-24 22:15:45 -0500

    Add Horoscope response

---
+++ app/src/main/java/com/example/horosapp/data/remote/horoscopemodule/response/HoroscopePredictionResponse.kt
@@ -0,0 +1,21 @@
+package com.example.horosapp.data.remote.horoscopemodule.response
+
+import com.example.horosapp.domain.features.horoscope.HoroscopePredictionModel
+import com.google.gson.annotations.SerializedName
+
+data class HoroscopePredictionResponse(
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("horoscope")
+    val horoscope: String,
+    @SerializedName("sign")
+    val sign: String
+) {
+
+    fun toDomain(): HoroscopePredictionModel {
+        return HoroscopePredictionModel(
+            horoscopePrediction = this.horoscope,
+            sign = this.sign
+        )
+    }
+}
