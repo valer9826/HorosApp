324fbd969b3c7de14d18428459eb9b3324fc3733
Renato Valer <valer9826@gmail.com>
2023-11-01 16:42:56 -0500

    Add HoroscopeDetailViewModel

--- app/src/main/java/com/example/horosapp/features/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/example/horosapp/features/detail/HoroscopeDetailViewModel.kt
@@ -1,17 +1,28 @@
 package com.example.horosapp.features.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.horosapp.domain.features.horoscope.HoroscopeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor(
+    private val repository: HoroscopeRepository
+): ViewModel() {
 
     private var _uiState = MutableStateFlow<HoroscopeDetailUiState>(HoroscopeDetailUiState.Loading)
     val uiState: StateFlow<HoroscopeDetailUiState> = _uiState
 
+
+    init {
+        viewModelScope.launch {
+//            repository.getHoroscopePrediction()
+        }
+    }
 
 
 }
